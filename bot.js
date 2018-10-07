da7c3d35e89893b94aeaeb99724c6134ece51a90
asWAEWQ <43911811+asWAEWQ@users.noreply.github.com>
2018-10-07 12:45:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -399,6 +399,28 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 
 
 
+client.on('ready', function(){
+    client.user.setStatus("dnd");
+    var ms = 100000 ;
+    var setGame = [`help Ninja Community Servers ${client.guilds.size} `,`$inv Users ${client.users.size}`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
+    }, ms);100000
+
+});
+
+
+
+
 
 
 
